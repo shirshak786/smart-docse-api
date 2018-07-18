633cb1e0f65365886e0702ce52432c378e0d3afc
Shirshak Bajgain <bloggervista@gmail.com>
2018-07-18 13:14:07 +0000

    Apply fixes from StyleCI

--- Modules/Result/Http/routes.php
+++ Modules/Result/Http/routes.php
@@ -1,6 +1,5 @@
 <?php
 
-Route::group(['middleware' => 'web', 'prefix' => 'result', 'namespace' => 'Modules\Result\Http\Controllers'], function()
-{
+Route::group(['middleware' => 'web', 'prefix' => 'result', 'namespace' => 'Modules\Result\Http\Controllers'], function () {
     Route::get('/', 'ResultController@index');
 });
