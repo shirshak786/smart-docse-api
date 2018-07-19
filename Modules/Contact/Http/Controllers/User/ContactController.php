32babd70456907ebe5fca3e63bcece086d99732e
Shirshak Bajgain <bloggervista@gmail.com>
2018-07-19 09:30:55 +0000

    Apply fixes from StyleCI

--- Modules/Contact/Http/Controllers/User/ContactController.php
+++ Modules/Contact/Http/Controllers/User/ContactController.php
@@ -2,10 +2,10 @@
 
 namespace Modules\Contact\Http\Controllers\User;
 
-use App\Http\Controllers\Controller;
-use Modules\Contact\Http\Requests\StoreContactRequest;
-use Modules\Contact\Models\Contact\Contact;
 use function response;
+use App\Http\Controllers\Controller;
+use Modules\Contact\Models\Contact\Contact;
+use Modules\Contact\Http\Requests\StoreContactRequest;
 
 class ContactController extends Controller
 {
@@ -17,8 +17,8 @@ class ContactController extends Controller
 
         return response()->json([
             'data' => [
-                'message' => 'Your message has succesfully been recieved. If we need to reply we will contact you via email you supplied. Thanks'
-            ]
+                'message' => 'Your message has succesfully been recieved. If we need to reply we will contact you via email you supplied. Thanks',
+            ],
         ])->setStatusCode(201);
     }
 }
